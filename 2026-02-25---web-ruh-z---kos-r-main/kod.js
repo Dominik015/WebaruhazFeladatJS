b67727c84ffe6d128919d264e0767302a3729e61
Dominik015 <158041727+Dominik015@users.noreply.github.com>
2026-02-27 09:10:20 +0100

    Add files via upload

--- 2026-02-25---web-ruh-z---kos-r-main/kod.js
+++ 2026-02-25---web-ruh-z---kos-r-main/kod.js
@@ -1,13 +1,25 @@
-function nagyitKep(melyikKep) {
-    document.getElementById("nagykep").src = melyikKep;
-}
-function visszaKep() {
-    document.getElementById("nagykep").src = "ures.jpg";
-}
-function kivalaszt(termekNev) {
-    let darabSzam = prompt("Hány darabot szeretnél vásárolni?");
-    let kosarLista = document.getElementById("kosarLista");
-    let uj = document.createElement("li");
-    uj.textContent = darabSzam + " db " + termekNev;
-    kosarLista.appendChild(uj);
+function szamol() {
+
+    var tabla = document.getElementById("tabla");
+    var sorok = tabla.getElementsByTagName("tr");
+
+    var vegosszeg = 0;
+
+    for (var i = 1; i < sorok.length; i++) {
+
+        var arCell = sorok[i].getElementsByClassName("ar")[0];
+        var osszegCell = sorok[i].getElementsByClassName("osszeg")[0];
+        var input = sorok[i].getElementsByTagName("input")[0];
+
+        var ar = parseInt(arCell.innerText);
+        var darab = parseInt(input.value);
+
+        var osszeg = ar * darab;
+
+        osszegCell.innerText = osszeg;
+
+        vegosszeg = vegosszeg + osszeg;
+    }
+
+    document.getElementById("vegosszeg").innerText = vegosszeg + " Ft";
 }
